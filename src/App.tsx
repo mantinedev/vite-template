052641235ffc135dd9d06a58ea8da130e9cc8dcc
Vitaly Rtishchev <rtivital@gmail.com>
2022-07-21 18:17:53 +0300

    Update dependencies to 5.0 alpha

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { MantineProvider, Text } from "@mantine/core";
+import { MantineProvider, Text, Button, Stack } from "@mantine/core";
 import { theme } from "./theme";
 
 export default function App() {
   return (
     <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
-      <Text>Welcome to Mantine!</Text>
+      <Stack align="center" mt={50}>
+        <Text size="xl" weight={500}>
+          Welcome to Mantine!
+        </Text>
+        <Button>Click the button</Button>
+      </Stack>
     </MantineProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,4 @@
-import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
