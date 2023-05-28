2f5fbebffcd05a10af965181ebbf1924512eb30a
Vitaly Rtishchev <rtivital@gmail.com>
2023-05-28 18:58:34 +0400

    Add storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
-import { Router } from './router';
+import { Router } from './Router';
 import { theme } from './theme';
 
 export default function App() {
