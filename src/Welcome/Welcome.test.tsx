2acb87787ac3b9717c213eb4d95d94073910e6cb
Vitaly Rtishchev <rtivital@gmail.com>
2023-09-18 17:57:30 +0400

    Update template to 7.0
    * Update template to v7 alpha
    
    * Add storybook
    
    * Update readme
    
    * Fix inaccurate welcome message description
    
    * Update @mantine/* dependencies to version 1.6.0
    
    * Update @mantine/* dependencies to version 7.0.0-beta.2
    
    * Update @mantine/* dependencies to version 7.0.0-beta.5
    
    * Update meta tags
    
    * Update @mantine/* dependencies to version 7.0.0-beta.7
    
    * Update @mantine/* dependencies to version 7.0.0

--- src/Welcome/Welcome.test.tsx
+++
@@ -1,8 +0,0 @@
-import { screen } from '@testing-library/react';
-import { render } from '@test-utils';
-import { Welcome } from './Welcome';
-
-it('displays welcome text', () => {
-  render(<Welcome />);
-  expect(screen.getByText('Welcome to Mantine!')).toBeInTheDocument();
-});
