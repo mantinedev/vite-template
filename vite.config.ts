83f22bb21e9064df0c47ae3286d946e34c79ea0e
Justin Chong <98152596+Justin-Metalynx@users.noreply.github.com>
2023-10-12 16:06:07 +0800

    Update vite.config.ts to support ts aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: '@test-utils', replacement: fileURLToPath(new URL('./test-utils', import.meta.url)) }
+    ],
+  }
 });
