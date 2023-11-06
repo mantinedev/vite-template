76d32b62032f20d8a5f5815bb7eab64e87274144
Vitaly Rtishchev <rtivital@gmail.com>
2023-11-06 16:35:27 +0400

    Init yarn 4 migration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.story.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-styling', 'storybook-dark-mode'],
+  addons: ['@storybook/addon-essentials', 'storybook-dark-mode'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
