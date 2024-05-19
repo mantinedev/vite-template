fa0f18e48056f8fb3033690919c44d3fec380873
Vitaly Rtishchev <rtivital@gmail.com>
2024-05-19 16:59:42 +0400

    Init initial configuration for eslit 9

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
 
+// Const a: Map<string, number> | null = new Map();
+
 export default function App() {
   return (
     <MantineProvider theme={theme}>
       <Router />
+      <button type={'submit'}>Just a button</button>
     </MantineProvider>
   );
 }
