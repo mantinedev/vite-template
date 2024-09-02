ca1f772173ad640aae00b25aaa264e9dbef01088
Vitaly Rtishchev <rtivital@gmail.com>
2024-09-02 14:58:27 +0400

    Cleanup eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,5 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   ...mantine,
-  // {
-  //   languageOptions: {
-  //     parserOptions: {
-  //       project: './tsconfig.json',
-  //     },
-  //   },
-  // },
   { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', './.storybook/main.ts'] },
 );
