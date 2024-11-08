9d7341bb07685b15adce02455df82c2c412af990
Vitaly Rtishchev <rtivital@gmail.com>
2024-11-08 13:18:13 +0400

    Add sass embedded example

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite';
+import path from 'node:path';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
@@ -8,5 +9,14 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.mjs',
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+        additionalData: `@use "${path.join(process.cwd(), 'src/_mantine')}" as mantine;`,
+      },
+    },
   },
 });
