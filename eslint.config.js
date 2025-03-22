42a920d16a78439d740b5e6a8c03061cfe9976ea
Vitaly Rtishchev <rtivital@gmail.com>
2025-03-22 18:39:50 +0400

    Disable no-console rule in storybook files

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,8 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   ...mantine,
   { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', './.storybook/main.ts'] },
+  {
+    files: ['**/*.story.tsx'],
+    rules: { 'no-console': 'off' },
+  }
 );
