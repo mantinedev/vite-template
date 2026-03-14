e7199b2f15b2bb0adf127b72b66a00c741c2bab1
Vitaly Rtishchev <rtivital@gmail.com>
2026-03-14 09:26:46 +0300

    Update to vite 8

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,15 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.mjs',
+  },
+
+  resolve: {
+    tsconfigPaths: true,
   },
 });
