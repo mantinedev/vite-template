03f53a90ecb4b60bce95a8b8489711dc497011c6
Vitaly Rtishchev <rtivital@gmail.com>
2026-07-19 13:05:08 +0300

    Fix formatting

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,3 @@
 import { createTheme } from '@mantine/core';
 
-export const theme = createTheme({
-  /** Put your mantine theme override here */
-});
+export const theme = createTheme({/** Put your mantine theme override here */});
